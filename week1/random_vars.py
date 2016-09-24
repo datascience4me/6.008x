7ae8b490cdfba28040fb83585cb43d8dd97ec89d
Anton Le <mail@antonle.me>
2016-09-24 12:46:36 +0300

    #1 6.008x-1 Complete week 1: - minor refactoring

--- week1/random_vars.py
+++ week1/random_vars.py
@@ -9,8 +9,11 @@ import comp_prob_inference
 
 
 def main():
-    prob_space = {'sunny': 1/2, 'rainy': 1/6, 'snowy': 1/3}
+    first_look()
 
+
+def first_look():
+    prob_space = {'sunny': 1 / 2, 'rainy': 1 / 6, 'snowy': 1 / 3}
     random_outcome = comp_prob_inference.sample_from_finite_probability_space(prob_space)
     W = random_outcome
     if random_outcome == 'sunny':
@@ -19,6 +22,7 @@ def main():
         I = 0
     print("Random variable W = " + W)
     print("Random variable I = " + str(I))
-        
+
+
 if __name__ == "__main__":
     main()

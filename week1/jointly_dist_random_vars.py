c94ebb5665c036fa9cc29a7b5df6c8d97412a63f
Anton Le <mail@antonle.me>
2016-09-24 15:33:47 +0300

    #1 Complete week 1: - adding simpsons_paradox_data.py - exercises for jointly_dist_random_vars done

--- week1/jointly_dist_random_vars.py
+++ week1/jointly_dist_random_vars.py
@@ -57,6 +57,43 @@ def approach_3():
     print(prob_W_T_array[prob_W_T_row_mapping['sunny'], prob_W_T_col_mapping['hot']])
 
 
+def ex_marginalization():
+    print('Exercise: Marginalization')
+    prob_I_W = {}
+    for w in {'sunny', 'rainy', 'snowy'}:
+        prob_I_W[w] = {}
+    prob_I_W['sunny'][1] = 1 / 2
+    prob_I_W['rainy'][0] = 1 / 6
+    prob_I_W['snowy'][0] = 1 / 3
+    print('Probability dict for I and W')
+    print(prob_I_W)
+
+    prob_X_Y = {}
+    for w in {'sunny', 'rainy', 'snowy'}:
+        prob_X_Y[w] = {}
+    prob_X_Y['sunny'][1] = 1 / 4
+    prob_X_Y['sunny'][0] = 1 / 4
+    prob_X_Y['rainy'][1] = 1 / 12
+    prob_X_Y['rainy'][0] = 1 / 12
+    prob_X_Y['snowy'][1] = 1 / 6
+    prob_X_Y['snowy'][0] = 1 / 6
+    print('Probability dict for X and Y')
+    print(prob_X_Y)
+
+    prob_X = {}
+    for key, prob_for_key in prob_X_Y.items():
+        prob_X[key] = sum(prob_for_key.values())
+    print('Probability dict for X random variable')
+    print(prob_X)
+
+    prob_Y = {0: 0, 1: 0}
+    for x, prob_for_x in prob_X_Y.items():
+        for y in prob_for_x:
+            prob_Y[y] += prob_for_x[y]
+    print('Probability dict for Y random variable')
+    print(prob_Y)
+
+
 def main():
     print('Jointly Distributed Random Variables')
     print('=====================================')
@@ -67,6 +104,8 @@ def main():
     approach_2()
     print('=====================================')
     approach_3()
+    print('=====================================')
+    ex_marginalization()
 
 
 if __name__ == "__main__":
